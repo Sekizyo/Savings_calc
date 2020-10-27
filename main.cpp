cce19da569b8ecffa39245156fc508757c35e321
Joseph <mateuszpieczulis123@gmail.com>
2020-10-27 16:55:30 +0100

    added menu

--- main.cpp
+++ main.cpp
@@ -62,8 +62,79 @@ void save_data(){ // Zapisanie wyniku do pliku
     int a = 0;
 }
 
+void choices(){ // Wybieranie trybu wprowadzania danych
+    // system("clear");
+    string mode;
+    string dynamic;
+
+    cout << "\nWybierz tryb wprowadzania danych: \n";
+    cout << "1. Zaladuj z pliku\n";
+    cout << "2. Wprowadz recznie\n";
+    cin >> mode;
+
+    if(mode == "1"){
+        load_config();
+    }
+    else if (mode == "2")
+    {
+        manual_config();
+    }
+    else
+    {
+        cout << "Wprowadz poprawną wartosc\n";
+        choices();
+    }
+
+    cout << "\nZmienne wartosci?\n";
+    cout << "t/n \n";
+    cin >> dynamic;
+
+    if(dynamic == "t"){
+        string wyb;
+        string month;
+
+        dynamic_values = true;
+
+        cout << "Ktora wartosc ma byc dynamiczna?\n";
+        cout << "1. Oprocentowanie\n";
+        cout << "2. Czestotliwosc kapitalizacji\n";
+        cout << "3. Miesieczna wplata\n";
+        cin >> wyb;
+
+        cout << "Od ktorego miesiaca wartosc ma sie zmienic?\n";
+        cin >> month;
+
+    }
+    else
+    {
+        return;
+    }
+}
+
+void calculate(){ // Właściwe obliczenia
+    int a = 0;
+}
+
+void draw_data(){ // Wypisz wprowadzone dane
+    // system("clear");
+    cout << "\nIlość miesięcy oszczędzania: " << miesiace << "\n";
+    cout << "Oprocentowanie środków (w skali rocznej): " << procent << "\n";
+    cout << "Częstotliwość kapitalizacji (co ile miesięcy): " << kapita << "\n";
+    cout << "Miesięczna wplata: " << wplata << "\n";
+    return;
+}
+
+void draw_menu(){
+    choices();
+    draw_data();
+    calculate();
+}
 
 int main(int argc, char const *argv[])
 {
+    bool running = true;
+    while(running == true){
+        draw_menu();
+    }
     return 0;
 }
