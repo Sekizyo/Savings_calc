39f671da8559e13a17115ff398e90f3e7466e36e
Joseph <mateuszpieczulis123@gmail.com>
2020-10-27 16:54:00 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int miesiace = 0; // Ilość miesięcy oszczędzania
+double procent = 0; // Oprocentowanie środków w skali rocznej
+int kapita = 0; // Częstotliwość kapitalizacji w miesiącach
+double wplata = 0; // Miesięczna wpłata
+bool dynamic_values = false;
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
